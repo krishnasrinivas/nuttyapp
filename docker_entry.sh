a25ddce881d48b6c8230587f0fb73e6db743eeab
Krishna Srinivas <krishna.srinivas@gmail.com>
2014-06-18 00:29:54 +0530

    not using supervisor

--- docker_entry.sh
+++ docker_entry.sh
@@ -4,7 +4,7 @@
 
 mongod --noprealloc --smallfiles --fork --logpath /var/log/mongodb/mongod.log
 service turnserver start
-supervisor peerjs -p 9000 &
-supervisor node /opt/bundle/main.js
+peerjs -p 9000 &
+node /opt/bundle/main.js
 
 
